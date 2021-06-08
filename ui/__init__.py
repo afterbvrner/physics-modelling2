f868682afc79d544b13186c1ce6e4415641723e1
Max <maksim.golish2002@gmail.com>
2021-06-08 19:29:02 +0300

    fix: Add substance dependence

--- ui/__init__.py
+++ ui/__init__.py
@@ -8,18 +8,20 @@ def neutron_interaction_options():
 
 if __name__ == "__main__":
     layout = [
-        [sg.Text("Select state of aggregation", font=("Arial", 15))],
-        [sg.Radio("solid", "SOARADIO", enable_events=True, default=True, key='solid', font=("Arial", 15))],
-        [sg.Radio("gas", "SOARADIO", enable_events=True, key='gas', font=("Arial", 15))],
-        [sg.Radio("liquid", "SOARADIO", enable_events=True, key='liquid', font=("Arial", 15))],
-        [sg.Text("Select interaction type", font=("Arial", 15))],
-        [sg.Combo(neutron_interaction_options(), key='-INTERACTIONSINPUT-', font=("Arial", 15))],
-        [sg.Text("Enter density", key='-FIRSTPARAM-', font=("Arial", 15))],
-        [sg.Input(key='-FIRSTPARAMINPUT-', font=("Arial", 15))],
-        [sg.Text("Enter molar mass of substance", key='-SECONDPARAM-', font=("Arial", 15))],
-        [sg.Input(key='-SECONDPARAMINPUT-', font=("Arial", 15))],
-        [sg.Text(size=(40, 1), key='-OUTPUT-', font=("Arial", 15))],
-        [sg.Button('Compute', font=("Arial", 15))]
+        [sg.Text("Select state of aggregation", font=("Arial", 14))],
+        [sg.Radio("solid", "SOARADIO", enable_events=True, default=True, key='solid', font=("Arial", 14))],
+        [sg.Radio("gas", "SOARADIO", enable_events=True, key='gas', font=("Arial", 14))],
+        [sg.Radio("liquid", "SOARADIO", enable_events=True, key='liquid', font=("Arial", 14))],
+        [sg.Text("Select interaction type", font=("Arial", 14))],
+        [sg.Combo(neutron_interaction_options(), key='-INTERACTIONSINPUT-', font=("Arial", 14))],
+        [sg.Text("Formula", key='-FORMULA-', font=("Arial", 14))],
+        [sg.Input(key='-FORMULAINPUT-', font=("Arial", 14))],
+        [sg.Text("p", key='-FIRSTPARAM-', font=("Arial", 14))],
+        [sg.Input(key='-FIRSTPARAMINPUT-', font=("Arial", 14))],
+        [sg.Text("", key='-SECONDPARAM-', font=("Arial", 14))],
+        [sg.Input(key='-SECONDPARAMINPUT-', font=("Arial", 14))],
+        [sg.Text(size=(40, 1), key='-OUTPUT-', font=("Arial", 14))],
+        [sg.Button('Compute', font=("Arial", 14))]
     ]
     window = sg.Window("Modelling", layout, size=(600, 400))
     while True:
@@ -27,23 +29,25 @@ if __name__ == "__main__":
         if event == sg.WINDOW_CLOSED:
             break
         if event == 'solid':
-            window['-FIRSTPARAM-'].update("Enter density")
-            window['-SECONDPARAM-'].update("Enter molar mass")
+            window['-FIRSTPARAM-'].update("p")
+            window['-SECONDPARAM-'].update("")
             continue
         if event == 'gas':
-            window['-FIRSTPARAM-'].update('Enter pressure')
-            window['-SECONDPARAM-'].update("Enter temperature")
+            window['-FIRSTPARAM-'].update('P')
+            window['-SECONDPARAM-'].update("T")
             continue
         if event == 'liquid':
-            window['-FIRSTPARAM-'].update("Enter density")
-            window['-SECONDPARAM-'].update("Enter molar mass")
+            window['-FIRSTPARAM-'].update("p")
+            window['-SECONDPARAM-'].update("")
             continue
 
         if values["gas"] == 'true':
             window['-OUTPUT-'].update(
                 str(length(
                     values['-INTERACTIONSINPUT-'],
-                    amount_of_gas(float(values['-FIRSTPARAMINPUT-']), float(values['-SECONDPARAMINPUT-'])))
+                    amount_of_gas(
+                        float(values['-FIRSTPARAMINPUT-']),
+                        float(values['-SECONDPARAMINPUT-'])) * sum(parse_amount_of_atoms(values['-FORMULAINPUT-'])))
                 )
                 + " centimeters"
             )
@@ -51,8 +55,10 @@ if __name__ == "__main__":
             window['-OUTPUT-'].update(
                 str(length(
                     values['-INTERACTIONSINPUT-'],
-                    amount_of_gas(float(values['-FIRSTPARAMINPUT-']), float(values['-SECONDPARAMINPUT-'])))
-                )
+                    amount_of_solid(
+                        float(values['-FIRSTPARAMINPUT-']),
+                        parse_molar_mass(values['-FORMULAINPUT-'])
+                    ) * sum(parse_amount_of_atoms(values['-FORMULAINPUT-']).values())))
                 + " centimeters"
             )
     window.close()
